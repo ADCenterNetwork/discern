7d8e5929f888925f6f56fe1439564ddfebd12c5f
DavidMinet <david.minet-casado-external@capgemini.com>
2020-11-13 14:11:54 +0100

    Docstrings added

--- generatorfind.py
+++ generatorfind.py
@@ -5,6 +5,14 @@ from ast2json import ast2json
 import time
 
 def get_name(node):
+    """get_name get_name will help us ocassionally to obtain the name that the node refers to.
+
+    Args:
+        node ([ast object]): [The ast node in which we are interested to get its name.]
+
+    Returns:
+        [string]: [The name that the node refers to.]
+    """
     try:
         x = node.func.id
         return x
@@ -25,6 +33,9 @@ def get_name(node):
 
 
 def saveast():
+    """saveast is a function that will create in our directory a .txt file with a pretty print of the tree. 
+    This file can help us to understand the structure of the ast.
+    """
     tree = ast.parse(open(sys.argv[1]).read())
     astprint = ast2json(tree)
     f = open("astree.txt", "w")
@@ -32,12 +43,28 @@ def saveast():
     f.close()
 
 def _get_folder(filename):
+    """_get_folder applies the os module to get the fullpath of a file. This is usefull because this program will open different files.
+
+    Args:
+        filename ([string]): [The name of the file we are interested.]
+
+    Returns:
+        [string]: [the full path of the file we are interested.]
+    """
     path = os.path.split(filename)[0]
     full_path = os.path.join(os.getcwd(), path)
     return full_path
 
 class Code():
+    """Code is a class that contains all the functions involved in the work with the ast of the file of interest.
+    """
     def __init__(self, name):
+        """we define some variables that are essential in the process of obtaining information and other variables 
+        that will store the information of interest.
+
+        Args:
+            name ([string]): [The name of the file that we are obtaining information when we execute generatorfind.py ]
+        """
         self.tree = ast.parse(open(name).read())
         self.generators = []
         self.path = name
@@ -45,6 +72,14 @@ class Code():
         self.assigns = {}
     
     def yieldfind(self, node = None, ls = []):
+        """Yieldfind search 'Yield's nodes and walk up the tree branch, saving all the nodes 
+        that contain that generator.
+
+        Args:
+            node ([ast object], optional): [A node through which we will travel to find the yield child node. 
+            At first, we will input the module object]. Defaults to None.
+            ls (list, optional): [List that we record the nodes we travel until find the yield node.]. Defaults to [].
+        """
         if node == None:
             node = self.tree
         if node.__class__.__name__ == 'Import':
@@ -66,6 +101,9 @@ class Code():
                         self.yieldfind(child, y)
 
     def generatorfind(self):
+        """generatorfind works with our list 'generators' in order to obtain the correct namespace instead of all the
+        nodes information.
+        """
         nodes=self.generators[:]
         for i in range(len(self.generators)):
             k = 0
@@ -88,6 +126,12 @@ class Code():
 
             
     def assign_call_find(self, node = None):
+        """assign_call_find is an idea to search the call to new assignments at the moment they are assigned
+         and it still is in development.
+
+        Args:
+            node ([], optional): []. Defaults to None.
+        """
         if node == None:
             node = self.tree
         for child in ast.iter_child_nodes(node):
@@ -96,11 +140,23 @@ class Code():
             #findcall()
         
     def assignsearch(self):
+        """assignsearch is a function that will search along the namespace of 'generators' and will call to __assignfind
+        in order to find if that element has been assigned as a new variable.
+        """
         for s in range(len(self.generators)):
                 self.__assignfind(self.tree, self.generators[s][:], self.generators[s], 0, s, [], []) 
         
     def __assignfind(self, node, item, sublista, i, s, *args, **kwargs):
+        """__assignfind will travel the branches of the tree in order to detect assignments to our element of interest
+        in the namespace of 'generators'.
 
+        Args:
+            node ([]): []
+            item ([]): []
+            sublista ([]): []
+            i ([]): []
+            s ([]): []
+        """
         if isinstance(node,  ast.ClassDef ) or isinstance(node,  ast.FunctionDef ) :
             return
             # Skip Classes and Functions 
@@ -146,6 +202,9 @@ class Code():
 
 
     def findcall(self):
+        """findcall will walk up the tree searching the calls to our different generators, and will allow us to record
+        some information about the call node, as for example the line of the call.
+        """
         for node in ast.walk(self.tree):
             if node.__class__.__name__ == 'Call':
                 for ls in self.generators:
@@ -223,21 +282,28 @@ def main(name):
     script = Code(name)
     saveast()      
     script.yieldfind()
+    print('-----------------------------------------------------------------------------------------------------\n')
+    print('In the following list we find the node\'s namespace of the generators defined in the script of interest:')
     for i in range(len(script.generators)):
         print('\n', i, ': \n', script.generators[i])
+    print("----------")
     script.generatorfind()
+    print('In the following list we find the namespace of the generators defined in the script of interest:')
     for i in range(len(script.generators)):
         print('\n', i, ': \n', script.generators[i])
     print("----------")
     script.assignsearch()
-    for i in range(len(script.generators)):
-        print('\n', i, ': \n', script.generators[i])
+    #for i in range(len(script.generators)):
+    #    print('\n', i, ': \n', script.generators[i])
+    #print("----------")
+
     script.findcall()
     print('LOS ASSIGNS SON LOS SIGUIENTES: ', script.assigns)
     print('LOS CALLS QUE HEMOS ENCONTRADO SON LOS SIGUIENTES: \n', script.calls)
     end = time.time()
     print("---------")
-    print(end-start)
+    print('Execution time:', end-start, 'seconds.')
+    print('-----------------------------------------------------------------------------------------------------\n')
     
 
 
