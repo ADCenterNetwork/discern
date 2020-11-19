6852b712a0345e90cbe1c166ccf75a927182302c
adomingu <alfonso.dominguez-de-la-iglesia-external@capgemini.com>
2020-11-19 08:26:11 +0100

    added tests for imports

--- tests/test_generatorfind.py
+++ tests/test_generatorfind.py
@@ -2,61 +2,76 @@ from ..generatorfind.generatorfind import Code
 import ast
 import os, pytest, shutil
 
-def onerror(func, path, exc_info):
-    """
-    Error handler for ``shutil.rmtree``.
 
-    From https://stackoverflow.com/a/2656405
-
-    If the error is due to an access error (read only file)
-    it attempts to add write permission and then retries.
-
-    If the error is for another reason it re-raises the error.
-
-    Usage : ``shutil.rmtree(path, onerror=onerror)``
-    """
-    import stat
-    if not os.access(path, os.W_OK):
-        # Is the error an access error ?
-        os.chmod(path, stat.S_IWUSR)
-        func(path)
-    else:
-        raise Exception("Cannot delete dir with shutil.rmtree")
-
-@pytest.fixture
-def workspace():
-    path = os.path.join(os.getcwd(), '__downloaded__')
-    if not os.path.isdir(path):
-        os.mkdir(path)
-    yield path  
-    try:
-        shutil.rmtree(path, onerror=onerror)
-    except Exception as ex:
-        print(ex)
 
 
 path = os.path.join(os.getcwd(), 'tests', 'pruebas.py')
 prueba = Code(path)
+
+path2 = os.path.join(os.getcwd(), 'tests', 'pruebas2.py')
+pruebas2 = Code(path2)
+
+
 
 yieldfind = prueba.yieldfind
 generatorfind = prueba.generatorfind
 assignsearch = prueba.assignsearch
 findcall = prueba.findcall
 
-
-def test_yieldfind():
+#@pytest.fixture
+def yieldnodes():
+    """yieldnodes works in pruebas.py: we detect its generators and we save the namespace's nodes of this 
+    generators.
+ 
+    Returns:
+        [list]: [list containing the node type of namespace's nodes.]
+    """
     yieldfind()
     res = []
     for sublist in prueba.generators:
         res.append([])
         for node in sublist:
             res[-1].append(node.__class__.__name__)
-        print(res)
+    return res
+
+
+def yieldnodes2():
+    """yieldnodes2 works in pruebas2.py: we detect its generators and we save the namespace's nodes of this 
+    generators.
+ 
+    Returns:
+        [list]: [list containing the node type of namespace's nodes.]
+    """
+    pruebas2.yieldfind()
+    res = []
+    for sublist in pruebas2.generators:
+        res.append([])
+        for node in sublist:
+            res[-1].append(node.__class__.__name__)
+    return res
+
+def test_yieldfind():
+    res = yieldnodes()
     assert res == [['Module', 'ClassDef', 'ClassDef', 'FunctionDef','While','Expr', 'Yield'], \
         ['Module', 'ClassDef', 'ClassDef', 'FunctionDef','While','Expr', 'Yield'],\
         ['Module', 'ClassDef', 'ClassDef', 'FunctionDef','While','Expr', 'Yield'], \
         ['Module','FunctionDef', 'FunctionDef', 'FunctionDef', 'While', 'Expr', 'Yield'], ['Module','FunctionDef','Expr', 'Yield']]
-        
+
+'''
+def test_yieldfind2():
+    """test_yieldfind2 asserts yieldnodes2 obtains the expected value. 
+    """
+    nodeclasses = yieldnodes2()
+    print(nodeclasses)
+    assert nodeclasses == [['Module','Module', 'Import', 'Module', 'ClassDef', 'ClassDef', 'FunctionDef','While','Expr', 'Yield'], \
+        ['Module','Module', 'Import', 'Module', 'ClassDef', 'ClassDef', 'FunctionDef','While','Expr', 'Yield'],\
+        ['Module','Module', 'Import', 'Module', 'ClassDef', 'ClassDef', 'FunctionDef','While','Expr', 'Yield'], \
+        ['Module','Module', 'Import', 'Module', 'FunctionDef', 'FunctionDef', 'FunctionDef', 'While', 'Expr', 'Yield'], \
+        ['Module','Module', 'Import', 'Module', 'FunctionDef','Expr', 'Yield'], \
+        ['Module','Module', 'Import', 'Module', 'Module', 'ClassDef', 'ClassDef', 'FunctionDef','Expr', 'Yield'], \
+        ['Module','Module', 'Import', 'Module', 'Module', 'FunctionDef','Expr', 'Yield']]
+        '''
+
 
 def test_generatorfind():
     generatorfind()
