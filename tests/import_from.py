b399c24dde9122d436e898a3adb9e95abf1eea54
adomingu <alfonso.dominguez-de-la-iglesia-external@capgemini.com>
2020-11-30 12:28:59 +0100

    changed tests for import_from

---
+++ tests/import_from.py
@@ -0,0 +1,18 @@
+#import generatorfind.generatorfind
+
+#import ..generatorfind
+
+from pruebas import Clase1_1, Clase2_1
+
+from import_test import import_test
+
+#import import_test.import_test
+
+from import_test.import_test import Clase
+
+from import_test.import_test import Clase
+
+Clase().funcion()
+
+
+#from generatorfind.generatorfind import Code
