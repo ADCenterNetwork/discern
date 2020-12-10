31d1c91fe9636606a59e31225f8c37e93aebca9b
DavidMinet <david.minet-casado-external@capgemini.com>
2020-12-10 11:12:45 +0100

    Bug with multiples imports solved.

--- generatorfind/generatorfind.py
+++ generatorfind/generatorfind.py
@@ -103,26 +103,23 @@ class Discern():
                             ls.append(item)
                     fileimp = fullpath+'.py'
                     treeimp = ast.parse(open(fileimp).read())
-                    self.__yieldfind(treeimp, ls) 
-                    break
-                else:
-                    if j == (len(importpath)-1):
-                        for root, directories, files in os.walk(fullpath):
-                            for filename in files:
-                                fileimp2 = os.path.join(fullpath, filename)
-                                if filename.endswith('.py'):
-                                    if node.names[i].asname:
-                                        ls.append(node.names[i].asname)
-                                    else:
-                                        filename2 = filename.split('.')
-                                        ls.append(importpath[j])
-                                        ls.append(filename2[0])
-                                    treeimp2 = ast.parse(open(fileimp2).read())
-                                    self.__yieldfind(treeimp2, ls)
-                    else:
-                        if not node.names[i].asname:
-                            pass
-                            ls.append(importpath[j])
+                    self.__yieldfind(treeimp, ls)
+                    for n in range(len(importpath)):
+                        ls.pop(0) 
+                    ls.pop(0)
+                else: #We are in a folder. We have to modify:
+                    for root, directories, files in os.walk(fullpath):
+                        for filename in files:
+                            fileimp2 = os.path.join(fullpath, filename)
+                            if filename.endswith('.py'):
+                                if node.names[i].asname:
+                                    ls.append(node.names[i].asname)
+                                else:
+                                    filename2 = filename.split('.')
+                                    ls.append(importpath[j])
+                                    ls.append(filename2[0])
+                                treeimp2 = ast.parse(open(fileimp2).read())
+                                self.__yieldfind(treeimp2, ls)
 
 
         if node.__class__.__name__ == 'ImportFrom':
