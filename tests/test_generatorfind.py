25a90cc44f85c8dba95ed9e1d61de4085ca0981f
DavidMinet <david.minet-casado-external@capgemini.com>
2020-12-11 12:49:42 +0100

    Test added

--- tests/test_generatorfind.py
+++ tests/test_generatorfind.py
@@ -1,4 +1,4 @@
-from ..generatorfind.generatorfind import Discern, FolderCalls
+from ..generatorfind.generatorfind import Discern, FolderCalls, Discern2
 import ast
 import os, pytest, shutil
 
@@ -26,6 +26,12 @@ def setup_multiple_assign():
 def setup_folder():
     path = os.path.join(os.getcwd(), 'tests', 'folder')
     prueba = FolderCalls(path)
+    return prueba
+
+@pytest.fixture
+def setup_discern2_pruebas2():
+    path = os.path.join(os.getcwd(), 'tests', 'pruebas2.py')
+    prueba = Discern2(path, [os.path.abspath("tests\\folder\\pruebas.py")])
     return prueba
 
 def test_namespace_pruebas(setup):
@@ -67,3 +73,9 @@ def test_generatorfind_folder(setup_folder):
         ('Clase1_1', 'Clase1_2', 'firstn'): [49, 56, 68, 77], ('Clase1_1', 'Clase1_3', 'firstn'): [62, 64], ('Clase2_1', 'Clase2_2', 'firstn'): [85, 89, 92, 93]},\
         'prueba_simple.py': {('Clase1', 'Clase2', 'f'): [7], ('f',): [7, 19, 20, 21]},\
         '__init__.py': {}}
+
+def test_discern_callsites_pruebas2(setup_discern2_pruebas2):
+    """test_callsites_imports will check that the calls to the generators are the expected with a specific
+    case in which generators are in other imported files.
+    """
+    assert setup_discern2_pruebas2.assign_call_find() ==  {('folder','pruebas', 'Clase1_1', 'Clase1_2', 'firstn'): [3]}
