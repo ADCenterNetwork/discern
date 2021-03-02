80d1773fa01f2ee4f6e33487d9c7ed87581026e2
DavidMinet <david.minet-casado-external@capgemini.com>
2021-03-02 10:55:22 +0100

    Refactoring discern2.py

--- generatorfind/discern2.py
+++ generatorfind/discern2.py
@@ -6,11 +6,12 @@ from .generatorfind import self_finder
 from .generatorfind import get_folder
 from .generatorfind import get_name
 
+
 class Discern2():
     """Discern2 is a class that contains all the functions involved in the work with the ast of the file of interest.
-	_ one inner level
-	__two inner levels
-	___three inner levels
+    _ one inner level
+    __two inner levels
+    ___three inner levels
     """
     def __init__(self, name, ls_modules):
         """we define some variables that are essential in the process of obtaining information and other variables 
@@ -43,20 +44,20 @@ class Discern2():
         # Iterate over nodes assigning an id to each one
         for node in ast.walk(self.tree):
             self.id[node] = i
-            i+=1
+            i += 1
 
     def sourcemapyield(self, node=None, ls=[]):
-        if node == None:
+        if node is None:
             node = self.tree
         if node.__class__.__name__ == 'Yield':
-                ls.append(node)
-                x = ls[:]
-                self.yieldslist.append(node)
+            ls.append(node)
+            x = ls[:]
+            self.yieldslist.append(node)
         else:
-                if ast.iter_child_nodes(node):
-                    for child in ast.iter_child_nodes(node):
-                        y = ls[:]
-                        self.sourcemapyield(child, y)
+            if ast.iter_child_nodes(node):
+                for child in ast.iter_child_nodes(node):
+                    y = ls[:]
+                    self.sourcemapyield(child, y)
 
         for nodoyield in self.yieldslist:
             self.yieldsdict[self.id[nodoyield]] = {"id": self.id[nodoyield], "col_offset": nodoyield.col_offset, "lineno": nodoyield.lineno}
@@ -71,69 +72,69 @@ class Discern2():
             At first, we will input the module object]. Defaults to None.
             ls (list, optional): [List that we record the nodes we travel until find the yield node.]. Defaults to [].
         """
-        if node == None:
+        if node is None:
             node = self.tree
 
         # If the node type is IMPORT we will parse that imported file and we will search generators on that file.
         if node.__class__.__name__ == 'Import':
-            self._register_generator_of_import_file(node, ls)
+            self.__register_generator_of_import_file(node, ls)
 
         # If the node type is IMPORTFROM we will parse that imported file and we will search generators on that file.
         if node.__class__.__name__ == 'ImportFrom':
-            self._register_generator_of_importfrom_file(node, ls)
+            self.__register_generator_of_importfrom_file(node, ls)
 
         # Yield node found, base case of recursion.
         if node.__class__.__name__ == 'Yield':
-            self._register_generator(node, ls)
+            self.__register_generator(node, ls)
 
         # We continue iterating until find a node of interest.
         else:
             if ast.iter_child_nodes(node):
-                self._iterate_over_child_nodes(node, ls)
+                self.__iterate_over_child_nodes(node, ls)
 
         return self.generators
 
-    def _register_generator_of_import_file(self, node, ls):
+    def __register_generator_of_import_file(self, node, ls):
         # We make a loop because we can import multiples files on a time.
         for i in range(len(node.names)):
             # We construct the path of the file imported in order to parse it.
             importpath = node.names[i].name.split('.')
-            absolute_path = os.path.join(os.getcwd(), self._fullpath_imported_file(node, ls, importpath))
-            if self.imported_file_correct(node, ls, importpath, absolute_path): 
+            absolute_path = os.path.join(os.getcwd(), self.__fullpath_imported_file(node, ls, importpath))
+            if self.__imported_file_correct(node, ls, importpath, absolute_path):
                 # In this case, we have imported a file that has generators inside. So, we parse it and search generators.
-                self._parse_and_register_gen(node, ls, importpath, i)
-            elif absolute_path in self.modules: #We are in a folder. We have to modify:
-                self._check_import_alias(node, ls, importpath, i)
+                self.__parse_and_register_gen(node, ls, importpath, i)
+            elif absolute_path in self.modules:  # We are in a folder. We have to modify:
+                self.__check_import_alias(node, ls, importpath, i)
                 # Calls to find yield commands in folders
                 self.__yieldfind_folders(absolute_path, ls)
 
-    def _parse_and_register_gen(self, node, ls, importpath, i):
-        self._check_import_alias(node, ls, importpath, i) #We check the asname (if exists) in order to register namespace.
-        fileimp = self._fullpath_imported_file(node, ls, importpath)+'.py'
+    def __parse_and_register_gen(self, node, ls, importpath, i):
+        self.__check_import_alias(node, ls, importpath, i) #We check the asname (if exists) in order to register namespace.
+        fileimp = self.__fullpath_imported_file(node, ls, importpath)+'.py'
         # Parse fileimp with AST library
         treeimp = ast.parse(open(fileimp, encoding="iso-8859-15", errors='ignore').read())
         # Recursive call in order to find  yield nodes in the imported file.
         self.__yieldfind(treeimp, ls)
         # We remove redundant elements.
-        [ ls.pop(0) for n in range(len(importpath)+1) ]
+        [ls.pop(0) for n in range(len(importpath)+1)]
 
-    def _check_import_alias(self, node, ls, importpath, i):
+    def __check_import_alias(self, node, ls, importpath, i):
         if node.names[i].asname:
             ls.append(node.names[i].asname)
         else:
             for item in importpath:
                 ls.append(item)
 
-    def imported_file_correct(self, node, ls, importpath, absolute_path):
-        return (os.path.isfile(self._fullpath_imported_file(node, ls, importpath)+'.py')  and (absolute_path+'.py' in self.modules))
+    def __imported_file_correct(self, node, ls, importpath, absolute_path):
+        return (os.path.isfile(self.__fullpath_imported_file(node, ls, importpath)+'.py')  and (absolute_path+'.py' in self.modules))
 
-    def _fullpath_imported_file(self, node, ls, importpath):
+    def __fullpath_imported_file(self, node, ls, importpath):
         fullpath = get_folder(self.path)
         for j in range(len(importpath)):
             fullpath = os.path.join(fullpath, importpath[j])
         return fullpath
 
-    def _register_generator_of_importfrom_file(self, node, ls):
+    def __register_generator_of_importfrom_file(self, node, ls):
         '''In a node like this one, the attribute 'module' contains the name of the left side (from left_side
         import right_side), in the same way it's represented in code (separated by dots)
 
@@ -144,82 +145,90 @@ class Discern2():
         will be on the right side
         '''
         getfolder = get_folder(self.path).split('/')
-        if node.module == None: #The importfrom has the structure: from . import file.
-            right_side = node.names
-            full_path2 = os.getcwd()
-            for item in getfolder:
-                full_path2 = os.path.join(full_path2, item)
-            full_path2 = full_path2.split('\\')
-            if node.level > 0:
-                for i in range(node.level-1):
-                    full_path2.pop(-1) 
-            full_path = full_path2[0]
-            full_path2.pop(0)
-            variable = 0
-            for item in full_path2:
-                if not variable == 0:
-                    full_path = os.path.join(full_path,item)
-                else:
-                    full_path = os.path.join(full_path + os.sep,item)
-                    variable+=1
-        else:                
-            left_side = node.module.split('.') 
-            right_side = node.names
-            #we now form a path from the elements on the left_side
-            full_path2 = os.getcwd()
-            for item in getfolder:
-                full_path2 = os.path.join(full_path2, item)
-            full_path2 = full_path2.split('\\')
-            if node.level >0:
-                for i in range(node.level-1):
-                    full_path2.pop(-1)
-            full_path = full_path2[0]
-            full_path2.pop(0)
-            variable=0
-            for item in full_path2:
-                if not variable == 0:
-                    full_path = os.path.join(full_path, item)
-                else:
-                    full_path = os.path.join(full_path + os.sep, item)
-                    variable+=1
-            for item in left_side:
-                full_path = os.path.join(full_path, item)
-        filename = full_path + '.py'
-        
-        if not filename in self.print:
-            #Uncomment next lines if you want to know the status of importfroms.
-            #print("* ImportFrom of", filename, "\n-> Enter:", filename in self.modules)
-            #print(filename)
-            self.print.append(filename)
-        if os.path.isfile(filename) and (filename in self.modules):
+        if node.module is None:  # The importfrom has the structure: "from . import file".
+            full_path = self.__construct_import_path_without_left_side(node, getfolder)
+            filename = full_path+'.py'
+        else:  # The importfrom has the structure "from .folder import filde".
+            full_path = self.__construct_import_path_with_left_side(node, getfolder)
+            filename = full_path + '.py'
+
+        if self.__importfrom_file_correct(filename):  # The imported file was on left_side, so we can parse it.
             tree2 = ast.parse(open(filename, encoding="iso-8859-15", errors='ignore').read())
-            # Recursive call
-            self.__yieldfind(tree2, ls)
-        #in this case, it means we have to access the right_side and look for files
-        else: 
-            for alias in right_side:
-                alias_filename = alias.name + '.py'
-                filename_path = os.path.join(full_path, alias_filename)
-                if os.path.isfile(filename_path) and (filename_path in self.modules):
-                    #we need to append the name of the file because that's how we'll call it in the function
-                    ls.append(alias) 
-                    tree2 = ast.parse(open(filename_path,encoding="iso-8859-15", errors='ignore').read())
-                    # Recursive call
-                    self.__yieldfind(tree2, ls)
+            self.__yieldfind(tree2, ls)  # Recursive call to yielfind.
+        else:  # In this case, it means we have to access the right_side and look for files.
+            self.__construct_path_with_right_side(node, full_path, ls)
 
+    def __construct_path_with_right_side(self, node, full_path, ls):
+        right_side = node.names
+        for alias in right_side:
+            alias_filename = alias.name + '.py'
+            filename_path = os.path.join(full_path, alias_filename)
+            if self.__importfrom_file_correct(filename_path):
+                # We append the alias to get the correct namespace.
+                ls.append(alias)
+                tree2 = ast.parse(open(filename_path, encoding="iso-8859-15", errors='ignore').read())
+                # Recursive call.
+                self.__yieldfind(tree2, ls)
 
-    def _register_generator(self, node, ls):
+    def __importfrom_file_correct(self, filename):
+        return os.path.isfile(filename) and (filename in self.modules)
+
+    def __construct_import_path_with_left_side(self, node, getfolder):
+        left_side = node.module.split('.')
+        # We now form a path from the elements on the left_side
+        full_path2 = self.__separate_path_elements(node, getfolder)
+        self.__check_relative_level(node, full_path2)
+        full_path = full_path2[0]
+        full_path2.pop(0)
+        variable = 0
+        # We have the path o a list, so on next line we join the elements of the lists with the os library. 
+        for item in full_path2:
+            if not variable == 0:
+                full_path = os.path.join(full_path, item)
+            else:
+                full_path = os.path.join(full_path + os.sep, item)
+                variable += 1
+        for item in left_side:
+            full_path = os.path.join(full_path, item)
+        return full_path
+
+    def __construct_import_path_without_left_side(self, node, getfolder):
+        full_path2 = self.__separate_path_elements(node, getfolder)
+        self.__check_relative_level(node, full_path2)
+        full_path = full_path2[0]
+        full_path2.pop(0)
+        variable = 0
+        for item in full_path2:
+            if not variable == 0:
+                full_path = os.path.join(full_path, item)
+            else:
+                full_path = os.path.join(full_path + os.sep, item)
+                variable += 1
+        return full_path
+
+    def __check_relative_level(self, node, full_path2):
+        if node.level > 0:
+            for i in range(node.level-1):
+                full_path2.pop(-1)
+
+    def __separate_path_elements(self, node, getfolder):
+        full_path2 = os.getcwd()
+        for item in getfolder:
+            full_path2 = os.path.join(full_path2, item)
+        full_path2 = full_path2.split('\\')
+        return full_path2
+
+    def __register_generator(self, node, ls):
         ls.append(node)
         x = ls[:]
-        self.generators.append(x)    
+        self.generators.append(x)
 
-    def _iterate_over_child_nodes(self, node, ls):
+    def __iterate_over_child_nodes(self, node, ls):
         ls.append(node)
         for child in ast.iter_child_nodes(node):
             y = ls[:]
             # Recursive call
             self.__yieldfind(child, y)
-
 
     def __yieldfind_folders(self, absolute_path, ls):
         init_filename = os.path.join(absolute_path, '__init__.py')
@@ -241,55 +250,69 @@ class Discern2():
 
     def _generatorfind(self):
         """_generatorfind() : works with our list 'generators' in order to
-                              obtain the correct namespace instead of all the nodes information.
+        obtain the correct namespace instead of all the nodes information.
         """
-        self.generators = []
-        # Search for all yield commands in "self.name"
+        self.generators = []  # Restart generator's list.
         self.generators = self.__yieldfind()
 
         # Iterates over the generated matrix
         for i in range(len(self.generators)):
             k = 0
-            for j in range(len(self.generators[i])-1): 
-                if  self.generators[i][-j-1].__class__.__name__ == "FunctionDef" :
+            for j in range(len(self.generators[i])-1):
+                if self.generators[i][-j-1].__class__.__name__ == "FunctionDef":
                     self.generators[i] = self.generators[i][1:-j]
                     break
             for m in range(len(self.generators[i])):
                 j = -m - 1
-                if not self.generators[i][j].__class__.__name__ =='Import' and not self.generators[i][j].__class__.__name__ =='Module' and not self.generators[i][j].__class__.__name__ =='If' and not self.generators[i][j].__class__.__name__ =='For' and not self.generators[i][j].__class__.__name__ =='If' and not self.generators[i][j].__class__.__name__ =='Try':
+                # If the node has name, we record it.
+                if self.__node_with_name(self.generators[i][j]):
                     if not type(self.generators[i][j]) == str:
                         self.generators[i][j] = self.generators[i][j].name
                 elif self.generators[i][j].__class__.__name__ == 'Module':
-                    k +=1
-                elif self.generators[i][j].__class__.__name__ =='Import':
-                    if self.generators[i][j].names[k-1].asname:
-                        self.generators[i][j] = self.generators[i][j].names[k-1].asname
-                    else:
-                        self.generators[i][j] = self.generators[i][j].names[k-1].name
+                    k += 1
+                elif self.generators[i][j].__class__.__name__ == 'Import':
+                    self.__record_import_name(i, j, k)
                 elif self.generators[i][j].__class__.__name__ == 'ImportFrom':
-                    if self.generators[i][j].names[k-1].asname:
-                        self.generators[i][j] = self.generators[i][j].names[k-1].asname
-                    elif self.generators[i][j].names[k-1].name:
-                        self.generators[i][j] = self.generators[i][j].names[k-1].name
-                elif self.generators[i][j].__class__.__name__ =='If':
+                    self.__record_importfrom_name(i, j, k)
+                elif self.generators[i][j].__class__.__name__ == 'If':
                     pass
-                elif self.generators[i][j].__class__.__name__ =='For':
+                elif self.generators[i][j].__class__.__name__ == 'For':
                     pass
-                elif self.generators[i][j].__class__.__name__ =='Try':
+                elif self.generators[i][j].__class__.__name__ == 'Try':
                     pass
-            self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'Module']
-            self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'If']
-            self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'For']
-            self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'Try']
-            #we add this last step to only get the unique elements
-            #because of how we construct this function, there may be repeated elements, which is redundant
+            self.__clean_namespace(i)
+
+        self.__remove_repeated_elements()
+        return self.generators
+
+    def __remove_repeated_elements(self):
         aux = []
         for ls in self.generators:
             if ls not in aux:
                 aux.append(ls)
         self.generators = aux[:]
-        return self.generators
-            
+
+    def __clean_namespace(self, i):
+        self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'Module']
+        self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'If']
+        self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'For']
+        self.generators[i] = [item for item in self.generators[i] if item.__class__.__name__ != 'Try']
+
+    def __record_importfrom_name(self, i, j, k):
+        if self.generators[i][j].names[k-1].asname:
+            self.generators[i][j] = self.generators[i][j].names[k-1].asname
+        elif self.generators[i][j].names[k-1].name:
+            self.generators[i][j] = self.generators[i][j].names[k-1].name
+
+    def __record_import_name(self, i, j, k):
+        if self.generators[i][j].names[k-1].asname:
+            self.generators[i][j] = self.generators[i][j].names[k-1].asname
+        else:
+            self.generators[i][j] = self.generators[i][j].names[k-1].name
+
+    def __node_with_name(self, node):
+        return (not node.__class__.__name__ == 'Import' and not node.__class__.__name__ == 'Module' and not node.__class__.__name__ == 'If' and not node.__class__.__name__ == 'For' and not node.__class__.__name__ == 'If' and not node.__class__.__name__ == 'Try')
+
     def assign_call_find(self, node = None):
         """assign_call_find is an idea to search the call to new assignments at the moment they are assigned
          and it still is in development.
